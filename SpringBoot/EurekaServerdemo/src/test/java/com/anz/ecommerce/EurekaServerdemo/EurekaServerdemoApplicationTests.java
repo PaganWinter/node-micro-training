0318e98e7d464611771a71798f8d429ae7c160b6
PaganWinter <whydieanut@gmail.com>
2017-09-08 15:30:56 +0530

    Add files via upload

---
+++ SpringBoot/EurekaServerdemo/src/test/java/com/anz/ecommerce/EurekaServerdemo/EurekaServerdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.anz.ecommerce.EurekaServerdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaServerdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
