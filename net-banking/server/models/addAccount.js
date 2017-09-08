392a4cc53cc2379dfc4c77a1c412db1e546a1966
PaganWinter <whydieanut@gmail.com>
2017-09-08 09:44:27 +0530

    Add files via upload

--- net-banking/server/models/addAccount.js
+++ net-banking/server/models/addAccount.js
@@ -4,17 +4,23 @@ module.exports = function(acctData) {
   var addAccountPromise = new Promise(function(resolve, reject) {
     var AccountModel = require('./schema').AccountModel
     var CustomerModel = require('./schema').CustomerModel
+
     // Get primary key for customer (_id) from customer.id
-    CustomerModel.findOne({customerRef: })
+    var custId = parseInt(acctData.customerId)
+    CustomerModel.findOne({id: custId}, function(err, custRecordRef) {
+      console.log("Found customer from custer.id", custRecordRef)
 
-    var custPrimKeyId = ''
+      var acctObj = new AccountModel({
+        num: acctData.num,
+        type: acctData.type,
+        balance: acctData.balance,
+        customerRef: custRecordRef
+      })
 
-    acctData['_id'] = custPrimKeyId
-    var acctObj = new AccountModel(acctData)
-
-    acctObj.save(function(err, result) {
-      if (err) reject(err)
-      return resolve(result)
+      acctObj.save(function(err, result) {
+        if (err) reject(err)
+        return resolve(result)
+      })
     })
   })
   return addAccountPromise
