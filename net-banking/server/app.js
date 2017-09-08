49d190aaa060c923736cba85409aeccc5419f78b
PaganWinter <whydieanut@gmail.com>
2017-09-08 11:20:37 +0530

    Add files via upload

--- net-banking/server/app.js
+++ net-banking/server/app.js
@@ -2,9 +2,11 @@ var express = require('express')
 var app = express()
 var bodyParser = require("body-parser")
 var path = require('path')
+var cors = require('cors')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 //app.use('/', express.static('../client/views'))
 //app.get('/', function (req, res) { res.send('Hello World!') })
